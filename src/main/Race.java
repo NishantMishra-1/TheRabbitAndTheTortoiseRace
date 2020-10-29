9c21bae2150bb5fa71a82461f61f4c702edd2503
My name <nishant.mishra_cs19@gla.ac.in>
2020-10-29 19:40:03 +0530

    create main method

--- src/main/Race.java
+++ src/main/Race.java
@@ -7,11 +7,8 @@
 
 package main;
 
-import thread.rabbit.Rabbit;
-
 public class Race {
     public static void main(String[] args) {
-        Rabbit rabbit = new Rabbit();
-        rabbit.getRabbit().start();
+
     }
 }
